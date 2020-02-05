b9b1b2f0e2e235bb8d49246ca34934c97d3a25a0
Alex Korotkov <programmerweb@yandex.ru>
2020-02-05 09:48:31 +0300

    Move patterns in an other folder

--- admin-dashboard/src/app/view/admin/admin.module.ts
+++ admin-dashboard/src/app/view/admin/admin.module.ts
@@ -5,10 +5,11 @@ import { SidebarComponent } from 'src/app/sidebar/sidebar.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { NewProjectComponent } from './new-project/new-project.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AdminComponent, SidebarComponent, ProjectsComponent, ProjectDetailsComponent, NewProjectComponent],
-  imports: [AdminRoutingModule],
+  imports: [AdminRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   entryComponents: []
 })
--- admin-dashboard/src/app/view/admin/new-project/new-project.component.ts
+++ admin-dashboard/src/app/view/admin/new-project/new-project.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-project',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-project.component.scss']
 })
 export class NewProjectComponent implements OnInit {
+  isShowError = false;
+  newProjectForm: FormGroup;
 
-  constructor() { }
+  constructor(private http: HttpClient, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.newProjectForm = this.fb.group({
+      name: ['', Validators.compose([Validators.required])],
+      version: ['', Validators.required],
+      typeMenu: [0]
+    });
   }
 
+  onSubmit() {
+    console.log('Your form data : ', this.newProjectForm.value);
+    this.isShowError = true;
+  }
 }
