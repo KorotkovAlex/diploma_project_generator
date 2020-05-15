96be510fc5ae4a928cf2e6e7a3e02bbb318271fb
Alex Korotkov <programmerweb@yandex.ru>
2020-05-15 14:34:02 +0300

    Add crud for contact-companies

--- backend-admin/src/contacts-companies/contacts-companies.controller.ts
+++ backend-admin/src/contacts-companies/contacts-companies.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { ContactCompany } from './contacts-company.entity';
+import { ContactsCompaniesService } from './contacts-companies.service';
 
+@Crud({
+  model: {
+    type: ContactCompany,
+  },
+})
 @Controller('contacts-companies')
-export class ContactsCompaniesController {}
+export class ContactsCompaniesController {
+  constructor(public service: ContactsCompaniesService) {}
+
+  get base(): CrudController<ContactCompany> {
+    return this;
+  }
+}
--- backend-admin/src/contacts-companies/contacts-companies.service.ts
+++ backend-admin/src/contacts-companies/contacts-companies.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { ContactCompany } from './contacts-company.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ContactsCompaniesService {}
+export class ContactsCompaniesService extends TypeOrmCrudService<
+  ContactCompany
+> {
+  constructor(
+    @InjectRepository(ContactCompany)
+    private readonly contactCompanyRepository: Repository<ContactCompany>,
+  ) {
+    super(contactCompanyRepository);
+  }
+}
