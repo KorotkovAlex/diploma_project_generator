5f65cec1dc77da9f2a9fb680cc318f9508410aea
Alex Korotkov <programmerweb@yandex.ru>
2020-05-15 14:39:06 +0300

    Add crud for services

--- backend-admin/src/services-companies/services-companies.controller.ts
+++ backend-admin/src/services-companies/services-companies.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { ServicesCompaniesService } from './services-companies.service';
+import { CrudController, Crud } from '@nestjsx/crud';
+import { ServicesCompany } from './services-company.entity';
 
+@Crud({
+  model: {
+    type: ServicesCompany,
+  },
+})
 @Controller('services-companies')
-export class ServicesCompaniesController {}
+export class ServicesCompaniesController {
+  constructor(public service: ServicesCompaniesService) {}
+
+  get base(): CrudController<ServicesCompany> {
+    return this;
+  }
+}
--- backend-admin/src/services-companies/services-companies.service.ts
+++ backend-admin/src/services-companies/services-companies.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { ServicesCompany } from './services-company.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ServicesCompaniesService {}
+export class ServicesCompaniesService extends TypeOrmCrudService<
+  ServicesCompany
+> {
+  constructor(
+    @InjectRepository(ServicesCompany)
+    private readonly aboutCompanyRepository: Repository<ServicesCompany>,
+  ) {
+    super(aboutCompanyRepository);
+  }
+}
