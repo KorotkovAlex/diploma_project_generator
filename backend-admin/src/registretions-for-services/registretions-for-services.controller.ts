465b248d3b80b63780387c53969cc2fc2d6bfeb4
Alex Korotkov <programmerweb@yandex.ru>
2020-05-15 14:36:26 +0300

    registrations for services

--- backend-admin/src/registretions-for-services/registretions-for-services.controller.ts
+++ backend-admin/src/registretions-for-services/registretions-for-services.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { RegistretionForService } from './registretions-for-services.entity';
+import { RegistretionsForServicesService } from './registretions-for-services.service';
 
+@Crud({
+  model: {
+    type: RegistretionForService,
+  },
+})
 @Controller('registretions-for-services')
-export class RegistretionsForServicesController {}
+export class RegistretionsForServicesController {
+  constructor(public service: RegistretionsForServicesService) {}
+
+  get base(): CrudController<RegistretionForService> {
+    return this;
+  }
+}
--- backend-admin/src/registretions-for-services/registretions-for-services.service.ts
+++ backend-admin/src/registretions-for-services/registretions-for-services.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { RegistretionForService } from './registretions-for-services.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class RegistretionsForServicesService {}
+export class RegistretionsForServicesService extends TypeOrmCrudService<
+  RegistretionForService
+> {
+  constructor(
+    @InjectRepository(RegistretionForService)
+    private readonly registretionForServiceRepository: Repository<
+      RegistretionForService
+    >,
+  ) {
+    super(registretionForServiceRepository);
+  }
+}
